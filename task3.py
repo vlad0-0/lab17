4b1a717bc4497d68312d5b8f3465ac9a9f8f4307
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-11-26 12:33:11 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,5 +4,27 @@ n = int(input())
 for i in range(n):
   a.append(int(input()))
 k1 = 1
-for i in range(n):
+l1 = 0
+r1 = 1
+i = 0
+while i < n-1:
   if a[i] == a[i+1]:
+    r1 += 1
+  else:
+    k1 += 1
+    l1 = r1
+    r1 += 1
+  i += 1
+  if k1 == k2:
+      break
+l2 = r1
+r2 = r1+1
+while i < n-1:
+  if a[i] == a[i+1]:
+    r2 += 1
+  else:
+    l2 = r2
+    r2 += 1
+  i += 1
+a[l1:r1+1], a[l2-1:r2] = a[l2-1:r2], a[l1:r1+1]
+print(a)
