6f30aa2a0bc14c4fdea6eee8483b15dbaff97719
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-11-26 11:08:15 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,14 @@
+a = []
+n = int(input())
+for i in range(n):
+  a.append(int(input()))
+b = []
+c = []
+l = 1
+for i in range(n-1)
+  if a[i] == a[i+1]:
+    l += 1
+  else:
+    b.append(a[i])
+    c.append(l)
+    l = 1
